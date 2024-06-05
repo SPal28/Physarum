13119ae40baba69cee8762c86038d4b6ca245a3b
SPal28 <ps247427@cpslions.com>
2024-06-05 18:08:49 -0400

    updated file path

--- extract.py
+++ extract.py
@@ -3,7 +3,7 @@ import cv2
 import os 
 
 # Read the video from specified path 
-cam = cv2.VideoCapture("C:\\Users\\Admin\\PycharmProjects\\project_1\\openCV.mp4") 
+cam = cv2.VideoCapture("C:\\Users\\subhangipal\\Documents\\Physarum\\video\\smooth_240104_1_MA062_01.mp4") 
 
 try: 
 	
