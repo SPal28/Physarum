7913c238a72bbb8428e68250ee8b95b2d161c580
SPal28 <ps247427@cpslions.com>
2024-06-05 19:41:32 -0400

    detect circles, but not showing image

--- locate.py
+++ locate.py
@@ -1,1 +1,19 @@
+import numpy as np 
+import cv2 as cv
+img = cv.imread('/Users/subhangipal/Documents/Physarum/physarum frames data/frame0.jpg')
+output = img.copy()
+
+#convert image to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+gray = cv.medianBlur(gray, 5)
+
+#using circles method
+circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+for (x, y, r) in circles:
+    cv2.circle(output, (x, y), 2, (0, 255, 0), 4)
+
+
+cv.imshow('output', output)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
