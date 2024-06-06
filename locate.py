bf3a9582963ef246f8b53bef4c2115a756718b43
SPal28 <ps247427@cpslions.com>
2024-06-06 16:19:02 -0400

    value

--- locate.py
+++ locate.py
@@ -9,8 +9,11 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 gray = cv.medianBlur(gray, 5)
 
 #using circles method from pyimagesearch
+
 circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
-for (x, y, r) in circles:
+detected_circles = np.uint16(np.around(circles))
+for value in detected_circles:
+    x, y, r = value
     cv.circle(output, (x, y), 2, (0, 255, 0), 4)
 
 
